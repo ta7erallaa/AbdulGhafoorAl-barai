6de4b826383f2020f066b3c968dc40cd60eb737b
taherallaa <ta7ermo2002@gmail.com>
2025-03-09 03:52:24 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      headerText: "وكالة عبد الغفور البرعي للبرمجيات",
+      overlayMessageRamadan: "وكالة عبد الغفور البرعي للبرمجيات تهنئكم بحلول شهر رمضان المبارك",
+      overlayMessage: "Stay tuned—exciting blogs on software engineering topics are coming sooooooooon!",
+    }
+  }
+}).mount('#app')

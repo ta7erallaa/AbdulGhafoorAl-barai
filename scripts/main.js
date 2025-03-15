7b1d73948080afbd5f9e7750f8a30fcc946873bc
taherallaa <ta7ermo2002@gmail.com>
2025-03-15 06:13:18 +0200

    ref(main.js): apply vue3 style (composition api).

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,14 @@
-const app = Vue.createApp({
-  data() {
+const { createApp, ref } = Vue;
+
+createApp({
+  setup() {
+    const headerText = ref("وكالة عبد الغفور البرعي للبرمجيات");
+    const overlayMessageRamadan = ref("وكالة عبد الغفور البرعي للبرمجيات تهنئكم بحلول شهر رمضان المبارك");
+    const overlayMessage = ref("Stay tuned—exciting blogs on software engineering topics are coming soooooon!");
     return {
-      headerText: "وكالة عبد الغفور البرعي للبرمجيات",
-      overlayMessageRamadan: "وكالة عبد الغفور البرعي للبرمجيات تهنئكم بحلول شهر رمضان المبارك",
-      overlayMessage: "Stay tuned—exciting blogs on software engineering topics are coming sooooooooon!",
+      headerText,
+      overlayMessage,
+      overlayMessageRamadan,
     }
   }
 }).mount('#app')
